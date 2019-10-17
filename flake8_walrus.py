817d948e1584447174ef7fac593a1a6db63ec9de
Anthony Sottile <asottile@umich.edu>
2019-10-17 16:10:12 -0700

    Use importlib.metadata in py38

--- flake8_walrus.py
+++ flake8_walrus.py
@@ -1,11 +1,10 @@
 import ast
+import importlib.metadata
 from typing import Any
 from typing import Generator
 from typing import List
 from typing import Tuple
 from typing import Type
-
-import importlib_metadata
 
 MSG = 'ASN001 do not use assignment expressions'
 
@@ -21,7 +20,7 @@ class Visitor(ast.NodeVisitor):
 
 class Plugin:
     name = __name__
-    version = importlib_metadata.version(__name__)
+    version = importlib.metadata.version(__name__)
 
     def __init__(self, tree: ast.AST):
         self._tree = tree
